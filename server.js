0bfeadc185b9c339e563468f2467d08f00d1cca9
Santiago Vizcarra <santiagomaticorena@mac.mynetworksettings.com>
2026-04-26 21:10:28 -0400

    Fix uploads foler for Render

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+if (!fs.existsSyns("uploads")) {
+	fs.mkdirSync("uploads");
+} 
 // Middleware
 app.use(cors());
 app.use(express.json());
